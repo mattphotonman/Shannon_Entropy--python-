3c51de8f0c6f555d023ce4b1d15390b44144ab7e
Matthew Lightman <mlightman@Matthew-Lightmans-MacBook-Pro.local>
2012-12-20 00:43:00 -0500

    Created the Queue class.

--- entropy.py
+++ entropy.py
@@ -14,13 +14,47 @@ character, and thus we calculate H/n.  We do this for
 several string lengths from 1 to n_max.
 """
 
+#-----------------------
 #Imports
+#-----------------------
 from math import *
+from UserList import UserList
+import copy
+import sys
 
+#-----------------------
 #Global variables
+#-----------------------
 
+
+#-----------------------
 #Classes
+#-----------------------
+
+class Queue:
+    """This is a special type of queue with a maximum
+    size n, and with methods that are particular to the
+    Shannon entropy code."""
+    def __init__(self,n=1):
+        self.n=n
+        self.data=[]
+
+    def Add(self,item):
+        self.data.append(item)
+        if len(self.data)<self.n:
+            return None
+        elif len(self.data)==self.n:
+            return copy.copy(self.data)
+        elif len(self.data)==self.n+1:
+            self.data.pop(0)
+            return copy.copy(self.data)
+        else:
+            print >> sys.stderr, "Error:  Queue became too large."
+            exit()
 
 
+
+#-----------------------
 #Main body of the code
+#-----------------------
 
