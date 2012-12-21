d054f8fde04a4851c0740ae797ff5af656e1bf21
Matthew Lightman <mlightman@Matthew-Lightmans-MacBook-Pro.local>
2012-12-21 00:26:14 -0500

    Fixed formatting of output columns.  Main code works.

--- entropy.py
+++ entropy.py
@@ -45,10 +45,10 @@ for line in f:
         stat.AddString(line)
 
 #Have all the data now, output the results.
-print "n\tNstrings\tEntropy\tSaturation Value\tEntropy Density\tSaturation Value"
+print "n\tNstrings\tEntropy\t\tSaturation Value\tEntropy Density\tSaturation Value"
 for stat in stats:
     ent=stat.ReturnEntropy()
     N=stat.ReturnN()
     ent_saturated=log(float(N))/log(2.0)
     #ent_saturated is the value the entropy would take if each string read were unique.
-    print stat.n, "\t", N, "\t", ent, "\t", ent_saturated, "\t", ent/float(stat.n), "\t", ent_saturated/float(stat.n)
+    print stat.n, "\t", N, "\t\t", ent, "\t", ent_saturated, "\t\t", ent/float(stat.n), "\t", ent_saturated/float(stat.n)
