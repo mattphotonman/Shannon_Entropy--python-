91aabf3da82e344b6b55b38e1a75e31542264602
Matthew Lightman <mlightman@Matthew-Lightmans-MacBook-Pro.local>
2012-12-20 21:46:16 -0500

    Modified main code so that you can specify nmin and nmax.

--- entropy.py
+++ entropy.py
@@ -11,27 +11,29 @@ We look at strings of length n, and thus i indexes all
 strings of length n that occur in the document.  We are
 interested in the entropy density, or information per
 character, and thus we calculate H/n.  We do this for
-several string lengths from 1 to n_max.
+several string lengths from nmin to nmax.
 """
 
 import sys
 from stringstat import *
 
 #Parse input
-if len(sys.argv)<2 or len(sys.argv)>3:
-    print >> sys.stderr, "Usage:  entropy.py filename [nmax=5]"
+if len(sys.argv)<2 or len(sys.argv)>4:
+    print >> sys.stderr, "Usage:  entropy.py filename [nmin=1] [nmax=5]"
     exit()
 
 filename=sys.argv[1]
-if len(sys.argv)==2:
-    nmax=5
-else:
-    nmax=int(sys.argv[2])
+nmin=1
+nmax=5
+if len(sys.argv)>2:
+    nmin=int(sys.argv[2])
+if len(sys.argv)>3:
+    nmax=int(sys.argv[3])
 
-#Create StringStat objects with n=1,2,...,nmax
+#Create StringStat objects with n=nmin,nmin+1,...,nmax
 stats=[]
-for i in range(nmax):
-    stats.append(StringStat(i+1))
+for n in range(nmin,nmax+1):
+    stats.append(StringStat(n))
 
 #Main loop of the program.
 #Read the lines of the file and feed them to
@@ -39,10 +41,10 @@ for i in range(nmax):
 #StringStat.AddString.
 f=open(filename,'r')
 for line in f:
-    for i in range(nmax):
-        stats[i].AddString(line)
+    for stat in stats:
+        stat.AddString(line)
 
 #Have all the data now, output the results.
 print "n\tEntropy Density"
-for i in range(nmax):
-    print stats[i].n, "\t", stats[i].ReturnEntropyDensity()
+for stat in stats:
+    print stat.n, "\t", stat.ReturnEntropyDensity()
